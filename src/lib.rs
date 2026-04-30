d65e5c80e2eb14717f722866690402ee966be1a7
Thomas Rask Thomsen <thomas_rask@hotmail.com>
2026-04-30 18:44:55 +0200

    Minor doc update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Proc macros for libraries that expose large collections through dynamic traits.
+//! Proc macros for libraries that expose internal types through dynamic traits.
 //!
 //! These macros allow library authors to keep internal implementations private while giving users the ability to:
 //! - Choose their own container type (`Box`, `Rc`, `Arc`, custom handles)
